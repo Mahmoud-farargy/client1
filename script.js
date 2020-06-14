71372bdf74e1096d4a330946ef136367f3952c01
Mahmoud-farargy <56843174+Mahmoud-farargy@users.noreply.github.com>
2020-06-15 01:52:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#switchToEnglishBtn").click(function(){
+        $(".main-ar").toggle();
+        $(".main-en").toggle();
+    })
+});
+
+console.log(document.title);
